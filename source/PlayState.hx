7c1e56fcf45d86bc8396e046105de02f89b47eee
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2022-06-23 04:07:49 -0400

    high score submitting workin nice

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -16,31 +16,38 @@ import flixel.util.FlxColor;
 import openfl.display.BitmapData;
 import openfl.utils.ByteArray;
 import openfl.utils.Object;
-
 import player.Player;
 
 class PlayState extends FlxState
 {
 	private var _player:Player;
+
 	public var player_start:FlxObject;
-	
+
 	private var _map:TiledLevel;
 	private var _mWalls:FlxTilemap;
-	
+
 	private var _timer:Float = 0;
 	private var _txtTimer:FlxText;
-	
+
 	private static var recording:Bool = false;
 	private static var replaying:Bool = false;
-	
+
 	private var endStuff:FlxGroup;
-	
+
 	override public function create():Void
 	{
+		#if newgrounds
+		if (!NGio.isLoggedIn)
+			var ng = new NGio(APIStuff.API, APIStuff.EncKey);
+		// NGio.init();
+		// NGio.login();
+		#end
+
 		FlxG.fixedTimestep = false;
-		
+
 		add(_player = Player.createPlayer());
-		
+
 		_map = new TiledLevel(AssetPaths.levelGood__tmx, this);
 		add(_map.backgroundLayer);
 		add(_map.imagesLayer);
@@ -48,74 +55,79 @@ class PlayState extends FlxState
 		add(_map.foregroundObjects);
 		add(_map.objectsLayer);
 		add(_map.collisionTiles);
-		
-		_map.foregroundTiles.forEach(function(t:FlxTilemap){t.visible = false; });
-		
+
+		_map.foregroundTiles.forEach(function(t:FlxTilemap)
+		{
+			t.visible = false;
+		});
+
 		var bg:FlxSprite = new FlxSprite().loadGraphic("assets/images/levelBG.png");
 		add(bg);
-		
+
 		FlxG.camera.follow(_player, FlxCameraFollowStyle.PLATFORMER);
 		_player.setPosition(player_start.x, player_start.y);
-		
+
 		FlxG.sound.playMusic(AssetPaths.ambience__ogg, 2.4);
-		
+
 		_txtTimer = new FlxText(20, 20, 0, "", 32);
 		_txtTimer.scrollFactor.set();
 		_txtTimer.visible = false;
 		add(_txtTimer);
-		
+
 		if (Main.fisrtRun)
 		{
-			FlxG.camera.fade(FlxColor.BLACK, 3, true, function(){Main.fisrtRun = false;});
+			FlxG.camera.fade(FlxColor.BLACK, 3, true, function()
+			{
+				Main.fisrtRun = false;
+			});
 		}
 
-		if (replaying) 
+		if (replaying)
 		{
 			persistentDraw = persistentUpdate = true;
 			openSubState(new EndState());
 		}
 		else
 		{
-			
 			// endStuff.visible = false;
 		}
-		
+
 		super.create();
 	}
+
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		
+
 		if (!recording && !replaying)
 		{
 			startRecording();
 		}
-		
+
 		FlxG.collide(_player, _map.foregroundTiles);
-		
+
 		#if FLX_KEYBOARD
 		if (FlxG.keys.justPressed.R && recording)
 		{
 			FlxG.vcr.stopRecording(false);
 			startRecording();
 		}
-		
+
 		if (FlxG.keys.justPressed.F && recording)
 		{
 			loadReplay();
 		}
 		#end
-		
+
 		// Time in milliseconds I think
 		_timer += FlxG.elapsed * 0.001;
-		
+
 		_txtTimer.text = FlxMath.roundDecimal(_timer * 1000, 2) + "s";
-		
-		
+
 		if (replaying)
 		{
 			_txtTimer.visible = true;
-			
+
 			#if FLX_KEYBOARD
 			if (FlxG.keys.pressed.SHIFT)
 			{
@@ -123,47 +135,49 @@ class PlayState extends FlxState
 			}
 			#end
 		}
-		
+
 		#if FLX_KEYBOARD
 		if (FlxG.keys.justPressed.T)
 		{
 			_txtTimer.visible = !_txtTimer.visible;
 		}
 		#end
-		
+
 		if (_player.x < 0 - _player.width)
 		{
 			FlxG.camera.fade(FlxColor.WHITE, 0.5, false, function()
 			{
 				loadReplay();
-				// FlxG.switchState(new EndState()); 
+
+				NGio.postScore(Std.int(_timer * 1000 * 1000), "Fastest Completion");
+
+				// FlxG.switchState(new EndState());
 			});
 		}
 	}
-	
+
 	private function startRecording():Void
 	{
 		recording = true;
 		replaying = false;
-		
+
 		FlxG.vcr.startRecording(false);
 	}
-	
+
 	private function loadReplay(dataOverride:String = ""):Void
 	{
 		EndState.time = FlxMath.roundDecimal(_timer * 1000, 2);
-		
+
 		replaying = true;
 		recording = false;
-		
+
 		var save = FlxG.vcr.stopRecording(false);
-		
-		
+
 		if (dataOverride.length > 0)
 		{
 			save = dataOverride;
 		}
-		
+
 		FlxG.vcr.loadReplay(save, new PlayState(), ["ENTER"], 0, startRecording);
 	}
 }
--- source/player/Player4Keys.hx
+++ source/player/Player4Keys.hx
@@ -1,8 +1,8 @@
 package player;
 
-import flixel.math.FlxMath;
-import flixel.FlxObject;
 import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.math.FlxMath;
 
 class Player4Keys extends Player
 {
@@ -12,115 +12,109 @@ class Player4Keys extends Player
 		var _down:Bool = false;
 		var _left:Bool = false;
 		var _right:Bool = false;
-		
+
 		_up = FlxG.keys.anyPressed([UP, W, SPACE]);
 		_down = FlxG.keys.anyPressed([DOWN, S]);
 		_left = FlxG.keys.anyPressed([LEFT, A]);
 		_right = FlxG.keys.anyPressed([RIGHT, D]);
-		
+
 		var _upR:Bool = false;
 		var _downR:Bool = false;
 		var _leftR:Bool = false;
 		var _rightR:Bool = false;
-		
+
 		_upR = FlxG.keys.anyJustReleased([UP, W, SPACE]);
 		_downR = FlxG.keys.anyJustReleased([DOWN, S]);
 		_leftR = FlxG.keys.anyJustReleased([LEFT, A]);
 		_rightR = FlxG.keys.anyJustReleased([RIGHT, D]);
-		
-		
+
 		var _upP:Bool = false;
 		var _downP:Bool = false;
 		var _leftP:Bool = false;
 		var _rightP:Bool = false;
-		
+
 		_upP = FlxG.keys.anyJustPressed([UP, W, SPACE]);
 		_downP = FlxG.keys.anyJustPressed([DOWN, S]);
 		_leftP = FlxG.keys.anyJustPressed([LEFT, A]);
 		_rightP = FlxG.keys.anyJustPressed([RIGHT, D]);
-		
-		
-		if ( velocity.x > 0)
+
+		if (velocity.x > 0)
 		{
 			facing = FlxObject.RIGHT;
 		}
-		if ( velocity.x < 0)
+		if (velocity.x < 0)
 		{
 			facing = FlxObject.LEFT;
 		}
-		
-		
+
 		if (_upR)
 		{
 			jumpingCooldown = 1;
 			jumpCounts = 0;
 		}
-		
+
 		if (justTouched(FlxObject.FLOOR))
 		{
 			FlxG.sound.play(AssetPaths.hitGround__wav, 0.7);
 		}
-		
+
 		if (isTouching(FlxObject.FLOOR))
 		{
-			
 			justJumped = false;
 			apexReached = false;
 			canJump = true;
 			canSideBoost = true;
 			sideBoosting = false;
 			jumpBoost = 0;
-			
+
 			if (_up)
 			{
 				jumpsTotal += 1;
-				
+
 				#if use_newgrounds_api
 				if (jumpsTotal == 7)
 				{
-					//API.unlockMedal("the jump man");
+					// API.unlockMedal("the jump man");
 				}
 				#end
-				
+
 				velocity.y -= (baseJumpStrength * 1.3);
-				
+
 				if (jumpCounts > 0)
 				{
 					jumpingCooldown = 0.8;
 				}
-				
+
 				jumpCounts += 1;
-				
-				
+
 				// old max height was 350;
 				if (canJump)
 				{
 					if (_left && _right)
 					{
 						_left = _right = false;
-						
 					}
 					else if (!_left && !_right)
 					{
 						velocity.x *= 0.77;
 						jumpedStraightUp = true;
 					}
-					
+
 					if (_left || _right)
 					{
 						jumpedStraightUp = false;
 						velocity.y -= (baseJumpStrength * 0.2);
 					}
-					
+
 					if (jumpedStraightUp)
 					{
 						sideBoost = false;
 					}
-					
+
 					var oldVel = velocity.x;
 					var reverseJumpMult:Float = 1.25;
 					var xSlowdown:Float = 1.2;
-					
+
 					if (_left)
 					{
 						velocity.x = -speed;
@@ -131,7 +125,7 @@ class Player4Keys extends Player
 							sideBoost = false;
 							sideBoosting = true;
 						}
-						
+
 						if (velocity.x - oldVel < velocity.x)
 						{
 							jumpingCooldown = reverseJumpMult;
@@ -139,11 +133,11 @@ class Player4Keys extends Player
 							FlxG.sound.play(AssetPaths.boost__wav, 0.7);
 						}
 					}
-					
+
 					if (_right)
 					{
 						velocity.x = speed;
-						
+
 						if (sideBoost && velocity.x + oldVel > velocity.x)
 						{
 							velocity.x = maxVelocity.x;
@@ -151,7 +145,7 @@ class Player4Keys extends Player
 							sideBoost = false;
 							sideBoosting = true;
 						}
-						
+
 						if (velocity.x + oldVel < velocity.x)
 						{
 							jumpingCooldown = reverseJumpMult;
@@ -159,28 +153,25 @@ class Player4Keys extends Player
 							FlxG.sound.play(AssetPaths.boost__wav, 0.7);
 						}
 					}
-					
+
 					justJumped = true;
 				}
-				
 			}
-			
+
 			if (!justJumped)
 				animation.play("stand");
 			else if (sideBoosting)
 				FlxG.sound.play(AssetPaths.sideJump__wav, 0.7);
 			else
 				FlxG.sound.play(AssetPaths.jumpBase__wav, 0.7);
-				
-			
+
 			drag.x = 1600 * 0.75;
 			acceleration.x = 0;
-			
+
 			if (sideBoosting)
 				maxVelocity.x = speed * 2;
 			else
 				maxVelocity.x = speed;
-		
 		}
 		else
 		{
@@ -188,23 +179,22 @@ class Player4Keys extends Player
 				animation.play("jump");
 			if (sideBoosting)
 				animation.play("dash");
-			
-			
+
 			if (_down)
 			{
 				apexReached = true;
-				velocity.y += 10;
+				velocity.y += 10 * (FlxG.elapsed / (1 / 60));
 			}
-			
+
 			if (isTouching(FlxObject.CEILING))
 			{
 				apexReached = true;
 			}
-			
+
 			if (_up && !apexReached && canJump)
 			{
 				jumpBoost += 1 * (FlxG.elapsed / (1 / 60));
-				
+
 				var C = FlxMath.fastCos(13 * jumpBoost * (1 / 60));
 				if (C < 0)
 				{
@@ -213,48 +203,47 @@ class Player4Keys extends Player
 				else
 				{
 					velocity.y -= (C * (baseJumpStrength * 1.2) * jumpingCooldown) * (FlxG.elapsed / (1 / 60));
-					//FlxG.watch.addQuick("cosine", C);
+					// FlxG.watch.addQuick("cosine", C);
 				}
 			}
-			
+
 			if (_upR)
 			{
 				apexReached = true;
 			}
-			
-			
+
 			if (_left && _right)
 			{
 				_left = _right = false;
 			}
-			
+
 			var accX:Float = 1.4;
-			
+
 			if (jumpedStraightUp || sideBoosting)
-				accX += 2.9 * (FlxG.elapsed / (1/60));
-			
+				accX += 2.9 * (FlxG.elapsed / (1 / 60));
+
 			if (!justJumped)
 			{
 				apexReached = true;
 				accX = 0.2;
 			}
-			
+
 			if (_left)
 			{
 				acceleration.x = -speed * accX;
 			}
-			
+
 			if (_right)
 			{
 				acceleration.x = speed * accX;
 			}
-			
+
 			if (_rightR || _leftR)
 			{
 				jumpedStraightUp = false;
 				acceleration.x = 0;
 			}
-			
+
 			if (canSideBoost)
 			{
 				if (velocity.x > 0 && _rightP && !jumpedStraightUp)
@@ -266,7 +255,7 @@ class Player4Keys extends Player
 					sideBoost = true;
 				}
 			}
-			
+
 			if (velocity.x > 0 && _leftP)
 			{
 				canSideBoost = false;
@@ -275,7 +264,7 @@ class Player4Keys extends Player
 			{
 				canSideBoost = false;
 			}
-			
+
 			drag.x = 0;
 		}
 	}
